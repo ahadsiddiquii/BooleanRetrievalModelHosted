766f36f01ab41789a56751b48cf14929e7845ea3
Ahad Siddiqui <ahad12siddiqui34@gmail.com>
2021-04-03 22:39:58 +0500

    fix static 2.2

--- BooleanRetrievalModel/settings.py
+++ BooleanRetrievalModel/settings.py
@@ -122,8 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 # Adding manually static directory
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+    if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+    else:
+        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
