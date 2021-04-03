990b364d8cffa964ba689581085de2985663c4fe
Ahad Siddiqui <ahad12siddiqui34@gmail.com>
2021-04-03 22:51:25 +0500

    fix static 2.5

--- BooleanRetrievalModel/settings.py
+++ BooleanRetrievalModel/settings.py
@@ -120,7 +120,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Adding manually static directory
